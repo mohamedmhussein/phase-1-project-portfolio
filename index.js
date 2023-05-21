b43cd0077c6b16c5697ba1e0b04196afe63f0b29
Mohamed Hussein <mohamedmhussein@protonmail.com>
2023-05-20 23:04:25 -0400

    Edits my name in the title section

--- index.js
+++ index.js
@@ -2,16 +2,17 @@
 
 // Clicking "about" subsection content (Skills, Experience, Education)
 const tabLink = document.querySelectorAll(".tabLink")
-tabLink.forEach(link => link.addEventListener("click",selectAboutSubSection))
+tabLink.forEach(link => link.addEventListener("click", selectAboutSubSection))
 
 // Fetching the services from the json server
 getAllServices()
 
 //Submit form into db.json file
-document.querySelector('form').addEventListener('submit',handleSubmit)
+document.querySelector('form').addEventListener('submit', handleSubmit)
 
 // ----------Functoins--------------
-function selectAboutSubSection(event){
+
+function selectAboutSubSection(event) {
     //remove active class of all subsection
     tabLink.forEach(link => link.classList.remove("active"))
 
@@ -20,19 +21,19 @@ function selectAboutSubSection(event){
 
     //remove active class from tabContent
     document.querySelectorAll('.tabContent').forEach(content => content.classList.remove("active"))
-    
+
     //add active class to only the content that is tied to the clicked subsection
     document.querySelector(`#${event.target.textContent}`).classList.add("active")
 
 }
 
-function getAllServices(){
+function getAllServices() {
     fetch("http://localhost:3000/services")
-    .then(res => res.json())
-    .then(services => services.forEach(service =>getOneService(service)))
+        .then(res => res.json())
+        .then(services => services.forEach(service => getOneService(service)))
 }
 
-function getOneService (service) {
+function getOneService(service) {
     let serviceElement = document.createElement('div')
     serviceElement.className = "service"
     serviceElement.innerHTML = `
@@ -43,7 +44,7 @@ function getOneService (service) {
     document.querySelector('.servicesList').append(serviceElement)
 }
 
-function handleSubmit(event){
+function handleSubmit(event) {
     event.preventDefault()
     let feedback = {
         name: event.target.name.value,
@@ -53,21 +54,21 @@ function handleSubmit(event){
     postFeedback(feedback)
 }
 
-function postFeedback(feedback){
-    fetch("http://localhost:3000/feedback",{
+function postFeedback(feedback) {
+    fetch("http://localhost:3000/feedback", {
         method: 'POST',
         headers: {
             'Content-Type': "application/json"
         },
         body: JSON.stringify(feedback)
     })
-    //showing "Message sent successfully" for few seconds after submitting the form
-    .then(res => {
-        let sent = document.querySelector('#sent')
-        sent.textContent = "Message sent successully"
-        setTimeout(function(){
-            sent.textContent = ""
-        },3000)
-        document.querySelector('form').reset() //resetting the form
-    })
+        //showing "Message sent successfully" for few seconds after submitting the form
+        .then(res => {
+            let sent = document.querySelector('#sent')
+            sent.textContent = "Message sent successully"
+            setTimeout(function () {
+                sent.textContent = ""
+            }, 3000) // remove message after 3 seconds
+            document.querySelector('form').reset() //resetting the form
+        })
 }
